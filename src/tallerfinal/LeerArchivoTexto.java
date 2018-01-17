a26d41db81cc906f606f35a31e1ba6bb275a4455
Artificial Intelligence Group from UTPL <inteligenciautpl@gmail.com>
2018-01-17 14:23:41 -0500

    Avance 1

--- src/tallerfinal/LeerArchivoTexto.java
+++ src/tallerfinal/LeerArchivoTexto.java
@@ -17,59 +17,72 @@ import java.util.Scanner;
  * @author Carlitos
  */
 public class LeerArchivoTexto {
-    
+
     private Scanner entrada;
+
     // permite al usuario abrir el archivo
-    public void abrirArchivo(){
-        try{
-            entrada = new Scanner( new File( "datos.txt" ) );
-        } // fin de try
-        catch ( FileNotFoundException fileNotFoundException ){
-            System.err.println( "Error al abrir el archivo." );
-            System.exit( 1 );
-        } // fin de catch
+    public void abrirArchivo(int op) {
+        if (op == 1) {
+            try {
+                entrada = new Scanner(new File("datos.txt"));
+            } // fin de try
+            catch (FileNotFoundException fileNotFoundException) {
+                System.err.println("Error al abrir el archivo.");
+                System.exit(1);
+            } // fin de catch
+        }
+        if (op == 2) {
+            try {
+                entrada = new Scanner(new File("datos.txt"));
+            } // fin de try
+            catch (FileNotFoundException fileNotFoundException) {
+                System.err.println("Error al abrir el archivo.");
+                System.exit(1);
+            } // fin de catch
+        }
     } // fin del metodo abrirArchivo
 
-   // lee registro del archivo
-    public int [] leerRegistros(){
+    // lee registro del archivo
+    public int[] leerRegistros() {
         ArrayList<Integer> lista = new ArrayList<>();
-        try{ // lee registros del archivo, usando el objeto Scanner
-            while ( entrada.hasNext() ){
+        try { // lee registros del archivo, usando el objeto Scanner
+            while (entrada.hasNext()) {
                 // System.out.println(entrada.nextLine());
                 String cadena = entrada.nextLine();
                 ArrayList<String> registro = new ArrayList<>(Arrays.asList(cadena.split(",")));
-                
+
                 int n1 = Integer.parseInt(registro.get(0));
-                lista.add(n1);                
+                lista.add(n1);
             } // fin de while
         } // fin de try
-        catch ( NoSuchElementException elementException ){
-            System.err.println( "El archivo no esta bien formado." );
+        catch (NoSuchElementException elementException) {
+            System.err.println("El archivo no esta bien formado.");
             entrada.close();
-            System.exit( 1 );
+            System.exit(1);
         } // fin de catch
-        catch ( IllegalStateException stateException ){
-            System.err.println( "Error al leer del archivo." );
-            System.exit( 1 );
+        catch (IllegalStateException stateException) {
+            System.err.println("Error al leer del archivo.");
+            System.exit(1);
         } // fin de catch
         return transformarArrayList_to_arreglo(lista);
     } // fin del m�todo leerRegistros
     // transforma de arraylist a arreglo
-    public int [] transformarArrayList_to_arreglo(ArrayList<Integer> lista){
-        int [] arreglo = new int[lista.size()];
+
+    public int[] transformarArrayList_to_arreglo(ArrayList<Integer> lista) {
+        int[] arreglo = new int[lista.size()];
         for (int i = 0; i < lista.size(); i++) {
             arreglo[i] = lista.get(i);
         }
         return arreglo;
     }
+
     // cierra el archivo y termina la aplicaci�n
-   public void cerrarArchivo(){
-        if ( entrada != null )
+    public void cerrarArchivo() {
+        if (entrada != null) {
             entrada.close(); // cierra el archivo
-   } // fin del m�todo cerrarArchivo
+        }
+    } // fin del m�todo cerrarArchivo
 }
-
-
 
 /**
  * ************************************************************************
@@ -83,5 +96,5 @@ public class LeerArchivoTexto {
  * el * editor no ser�n responsables en ning�n caso por los da�os consecuentes *
  * en conexi�n con, o que surjan de, el suministro, desempe�o o uso de * estos
  * programas. *
- ************************************************************************
+ * ***********************************************************************
  */
--- src/tallerfinal/TallerFinal.java
+++ src/tallerfinal/TallerFinal.java
@@ -12,15 +12,17 @@ import java.util.Scanner;
  * @author Carlitos
  */
 public class TallerFinal {
-
+     static OrdenamientoSeleccion arreglos;
+    //static LeerArchivoTexto leeA;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sn = new Scanner(System.in);
+        int ops = 0;
         LeerArchivoTexto leeA = new LeerArchivoTexto();
-        leeA.abrirArchivo();
+        
 
         boolean salir = false;
         int opcion; //Guardaremos la opcion del usuario
@@ -28,13 +30,177 @@ public class TallerFinal {
         while (!salir) {
             System.out.println(" ESCOJA UNA OPCION");
 
-            System.out.println("1: Ordenamiento Por combinacion");
-            System.out.println("2: Ordenamiento Por Inserccion");
-            System.out.println("3: SALIR");
+            System.out.println("1: Ordenamiento Por Seleccion");
+            System.out.println("2: Ordenamiento Por combinacion");
+            System.out.println("3: Ordenamiento Por Inserccion");
+            System.out.println("4: SALIR");
             try {
                 opcion = sn.nextInt();
                 switch (opcion) {
                     case 1:
+                        long startTime;
+                        long endTime;
+                        int op;
+
+                        System.out.println("INGRESE LA OPCION CON EL NUMERO DE VALORES A OPERAR");
+                        System.out.println("1: (100)");
+                        System.out.println("2: (1000)");
+                        System.out.println("3: (5000)");
+                        System.out.println("4: (1000)");
+                        System.out.println("5: (50000)");
+                        System.out.println("6: (100000)");
+                        System.out.println("7: (500000)");
+                        System.out.println("8: (1000000)");
+                        System.out.println("9: (2000000)");
+                        System.out.println("10: SALIR");
+
+                        op = sn.nextInt();
+                        switch (op) {
+                            case 1:
+                                startTime = System.nanoTime();
+                                leeA.abrirArchivo(op);
+                                arreglos = new OrdenamientoSeleccion(leeA.leerRegistros());
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+                                break;
+                            case 2:
+
+                                startTime = System.nanoTime();
+
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+                                break;
+                            case 3:
+
+                                startTime = System.nanoTime();
+
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+                                break;
+                            case 4:
+
+                                startTime = System.nanoTime();
+
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+                                break;
+                            case 5:
+
+                                startTime = System.nanoTime();
+
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+                                break;
+                            case 6:
+
+                                startTime = System.nanoTime();
+
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+                                break;
+                            case 7:
+                                startTime = System.nanoTime();
+
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+                                break;
+                            case 8:
+                                startTime = System.nanoTime();
+
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+
+                                break;
+                            case 9:
+                                startTime = System.nanoTime();
+
+                                System.out.println("Arreglo desordenado:");
+                                System.out.println(arreglos); // imprime arreglo desordenado
+
+                                arreglos.ordenar(); // ordena el arreglo
+
+                                System.out.println("Arreglo ordenado:");
+                                System.out.println(arreglos); // imprime el arreglo ordenado
+                                leeA.cerrarArchivo();
+                                endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                                System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
+
+                                break;
+                            case 10:
+                                salir = true;
+                                break;
+                            default:
+                                System.out.println("Ingrese un Numero Valido");
+
+                        }
+                        break;
+                    case 2:
+                        startTime = System.nanoTime();
+
                         OrdenamientoCombinacion arregloOrden = new OrdenamientoCombinacion(leeA.leerRegistros());
 
                         // imprime el arreglo desordenado
@@ -44,8 +210,13 @@ public class TallerFinal {
 
                         // imprime el arreglo ordenado
                         System.out.println("Ordenado:   " + arregloOrden);
+                        leeA.cerrarArchivo();
+                        endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                        System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
                         break;
-                    case 2:
+                    case 3:
+                        startTime = System.nanoTime();
+
                         OrdenamientoInsercion arreglo = new OrdenamientoInsercion(leeA.leerRegistros());
 
                         System.out.println("Arreglo desordenado:");
@@ -55,8 +226,11 @@ public class TallerFinal {
 
                         System.out.println("Arreglo ordenado:");
                         System.out.println(arreglo);
+                        leeA.cerrarArchivo();
+                        endTime = System.nanoTime() - startTime; // tiempo en que se ejecuta su método
+                        System.out.println("Tiempo" + "Duración: " + (endTime) / 1e6 + " ms");
                         break;
-                    case 3:
+                    case 4:
                         salir = true;
                         break;
                     default:
@@ -68,10 +242,9 @@ public class TallerFinal {
                 System.out.println("Debes insertar un número");
                 sn.next();
             }
-            
+
         }
 
-       
     }
 
 }
